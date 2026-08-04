4b1a90ac7617299c15c085538191dc2ed8996370
mdavo5270-create <mdavo5270@gmail.com>
2026-08-04 11:49:30 +0100

    feat: Controller

---
+++ src/pokemon_game/core/controller.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Controller:
+    """Controller class to manage the keys."""
+
+    def __init__(self):
+        self.keys: dict[str, int] = {
+            "up": pygame.K_z,
+            "down": pygame.K_s,
+            "left": pygame.K_q,
+            "right": pygame.K_d,
+            "action": pygame.K_e,
+            "bike": pygame.K_b,
+            "quit": pygame.K_ESCAPE,
+        }
+
+    def get_key(self, key: str) -> int:
+        return self.keys[key]
+
+    def add_key(self, key: str, value: int) -> None:
+        self.keys[key] = value
